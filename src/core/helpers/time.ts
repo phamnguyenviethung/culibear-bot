2743c549a7c468f947de0de7f68ff7250c1599dd
Viet Hung <viethung01234@gmail.com>
2026-01-25 08:21:10 +0700

    Update dayjs locale import to use .js extension for ES module compatibility

--- src/core/helpers/time.ts
+++ src/core/helpers/time.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc.js';
 import timezone from 'dayjs/plugin/timezone.js';
-import 'dayjs/locale/vi';
+import 'dayjs/locale/vi.js';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
