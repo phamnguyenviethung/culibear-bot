5e301ebf46b64c292806ad65f7574b607f20bcf6
Viet Hung <viethung01234@gmail.com>
2026-01-28 06:41:58 +0700

    Add logging notification for bot readiness in production environment
    - Enhanced the `ready.event.ts` to send a message to a specified log channel when the bot is ready, but only in production mode.
    - Integrated `appConfig` and `discordMessageService` to facilitate the message sending functionality.

--- src/bot/events/ready.event.ts
+++ src/bot/events/ready.event.ts
@@ -1,6 +1,12 @@
 import logger from '@/configs/logger.config.js';
 import { BotClient } from '../index.js';
+import appConfig from '@/configs/app.config.js';
+import discordMessageService from '@/core/service/discordMessage.service.js';
 
 export default async (client: BotClient) => {
   logger.info(client.user?.username + ' đã sẵn sàng');
+  if (appConfig.bot.env === 'production' && process.env.LOG_CHANNEL_ID) {
+    const msg = `@here ${client.user?.username} đã sẵn sàng`;
+    await discordMessageService.sendMessageToChannel(client, process.env.LOG_CHANNEL_ID as string, msg);
+  }
 };
