0c6687cf9b4f7a44018bb95dd31d91a96578d490
Viet Hung <viethung01234@gmail.com>
2026-01-30 06:06:11 +0700

    feat: add reminder job to cron scheduler

--- src/core/cron/index.ts
+++ src/core/cron/index.ts
@@ -1,7 +1,9 @@
 import logger from '@/configs/logger.config.js';
 import birthdayJob from './birhtday.job.js';
+import remindJob from './remind.job.js';
 
 export default () => {
   birthdayJob();
+  remindJob();
   logger.info('Cron jobs started');
 };
